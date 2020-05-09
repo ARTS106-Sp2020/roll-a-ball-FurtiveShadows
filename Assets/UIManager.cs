05f0b4bc4edd95f715306c7a86c951555bd2cd3c
FurtiveShadows <61507092+FurtiveShadows@users.noreply.github.com>
2020-05-08 21:53:31 -0400

    Added Title Menu

---
+++ Assets/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Roll-a-ball");
+	}
+	
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+	
+}
